5994ecb5aa9384f80716139a8a13f31563c85d7b
maple <maple@itcast.cn>
2018-09-15 18:10:37 +0800

    商家审核  状态修改完成

--- pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/SellerController.java
+++ pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/SellerController.java
@@ -110,5 +110,21 @@ public class SellerController {
 	public PageResult search(@RequestBody TbSeller seller, int page, int rows  ){
 		return sellerService.findPage(seller, page, rows);		
 	}
+	/**
+	 * 状态修改  
+	 * @param sellerId
+	 * @param status
+	 * @return
+	 */
+	@RequestMapping("/updateStatus")
+	public Result updateStatus(String sellerId,String status) {
+		try {
+			sellerService.updateStatus(sellerId, status);
+			return new Result(true, "更改成功");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Result(false, "更改失败");
+		}
+	}
 	
 }
--- pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/SellerService.java
+++ pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/SellerService.java
@@ -57,5 +57,11 @@ public interface SellerService {
 	 * @return
 	 */
 	public PageResult findPage(TbSeller seller, int pageNum,int pageSize);
+	/**
+	 * 更改状态--商家审核处理
+	 * @param sellerId
+	 * @param status
+	 */
+	void updateStatus(String sellerId,String status);
 	
 }
