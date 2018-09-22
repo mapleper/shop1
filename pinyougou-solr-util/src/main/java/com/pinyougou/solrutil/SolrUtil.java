b2aa3b51b1893274ad56326c8885bc8aa7c07468
maple <maple@itcast.cn>
2018-09-22 19:03:38 +0800

    spring data solr   基本数据导入Sorl索引库完成

---
+++ pinyougou-solr-util/src/main/java/com/pinyougou/solrutil/SolrUtil.java
@@ -0,0 +1,47 @@
+package com.pinyougou.solrutil;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.solr.core.SolrTemplate;
+import org.springframework.stereotype.Component;
+
+import com.pinyougou.mapper.TbItemMapper;
+import com.pinyougou.pojo.TbItem;
+import com.pinyougou.pojo.TbItemExample;
+import com.pinyougou.pojo.TbItemExample.Criteria;
+
+@Component
+public class SolrUtil {
+	@Autowired
+	private TbItemMapper itemMapper;
+	@Autowired
+	private SolrTemplate solrTemplate;
+	/**
+	 * 导入商品sku数据 
+	 */
+	public void importItemData() {
+		TbItemExample example = new TbItemExample();
+		Criteria criteria = example.createCriteria();
+		//只查出已审核的数据
+		criteria.andStatusEqualTo("1");
+		
+		List<TbItem> list = itemMapper.selectByExample(example);
+		
+		for (TbItem tbItem : list) {
+			System.out.println(tbItem.getTitle());
+		}
+		solrTemplate.saveBeans(list);
+		solrTemplate.commit();
+		
+	}
+	public static void main(String[] args) {
+		ApplicationContext context=new ClassPathXmlApplicationContext("classpath*:spring/applicationContext*.xml");
+		SolrUtil sorlUtil= (SolrUtil) context.getBean("solrUtil");
+		sorlUtil.importItemData();
+				
+	}
+
+}
