548760b0dc7ba2c01ba4dbab294e4c6063faf9c2
maple <maple@itcast.cn>
2018-09-28 13:24:09 +0800

    商品详细页--分类面包屑

--- pinyougou-page-service/src/main/java/com/pinyougou/page/service/impl/ItemPageServiceImpl.java
+++ pinyougou-page-service/src/main/java/com/pinyougou/page/service/impl/ItemPageServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.web.servlet.view.freemarker.FreeMarkerConfig;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.pinyougou.mapper.TbGoodsDescMapper;
 import com.pinyougou.mapper.TbGoodsMapper;
+import com.pinyougou.mapper.TbItemCatMapper;
 import com.pinyougou.page.service.ItemPageService;
 import com.pinyougou.pojo.TbGoods;
 import com.pinyougou.pojo.TbGoodsDesc;
@@ -33,6 +34,8 @@ public class ItemPageServiceImpl implements ItemPageService {
 	private TbGoodsMapper goodsMapper;
 	@Autowired
 	private TbGoodsDescMapper goodsDescMapper;
+	@Autowired
+	private TbItemCatMapper itemCatMapper;
 	@Override
 	public boolean genItemHtml(Long goodsId) {
 		Configuration configuration = freeMarkerConfig.getConfiguration();
@@ -45,6 +48,13 @@ public class ItemPageServiceImpl implements ItemPageService {
 			//2.加载商品描述表数据
 			TbGoodsDesc goodsDesc = goodsDescMapper.selectByPrimaryKey(goodsId);
 			dataModel.put("goodsDesc", goodsDesc);
+			//3.商品分类信息
+			String itemCat1 = itemCatMapper.selectByPrimaryKey(goods.getCategory1Id()).getName();
+			String itemCat2 = itemCatMapper.selectByPrimaryKey(goods.getCategory2Id()).getName();
+			String itemCat3 = itemCatMapper.selectByPrimaryKey(goods.getCategory3Id()).getName();
+			dataModel.put("itemCat1", itemCat1);
+			dataModel.put("itemCat2", itemCat2);
+			dataModel.put("itemCat3", itemCat3);
 			
 			Writer out=new FileWriter(pagedir+goodsId+".html");//指定写到的路径及文件名
 			template.process(dataModel, out);
