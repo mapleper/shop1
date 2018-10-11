8884c9bbae2961c9b8321e524a0c5a58d72c4b73
maple <maple@itcast.cn>
2018-10-11 18:20:37 +0800

    购物车服务--统计数  商品总数  共计价格ok

--- pinyougou-cart-web/src/main/webapp/js/service/cartService.js
+++ pinyougou-cart-web/src/main/webapp/js/service/cartService.js
@@ -9,4 +9,20 @@ app.service('cartService',function($http){
 	this.addGoodsToCartList=function(itemId,num){
 		return $http.get('cart/addGoodsToCartList.do?itemId='+itemId+'&num='+num);
 	}
+	
+	//求合计数  合计商品数量  合计商品费用
+	this.sum=function(cartList) {
+		var totalValue={totalNum:0,totalMoney:0.00};//合计实体  便于数据返回
+		for(var i=0;i<cartList.length;i++) {
+			var cart=cartList[i];
+			for(var j=0;j<cart.orderItemList.length;j++) {
+				var orderItem=cart.orderItemList[j];
+				totalValue.totalNum+=orderItem.num;
+				totalValue.totalMoney+=orderItem.totalFee;
+			}
+			
+		}
+		return totalValue;
+		
+	}
 });
