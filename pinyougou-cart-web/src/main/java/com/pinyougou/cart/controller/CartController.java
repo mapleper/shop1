919fccf2b6c1fc80e3b69c422b6043171c33f8ec
maple <maple@itcast.cn>
2018-10-12 13:14:36 +0800

    从商品详细页  点击加入购物车  跨域请求  cart域ok

--- pinyougou-cart-web/src/main/java/com/pinyougou/cart/controller/CartController.java
+++ pinyougou-cart-web/src/main/java/com/pinyougou/cart/controller/CartController.java
@@ -67,6 +67,9 @@ public class CartController {
 	}
 	@RequestMapping("/addGoodsToCartList")
 	public Result addGoodsToCartList(Long itemId,Integer num) {
+		response.setHeader("Access-Control-Allow-Origin", "http://localhost:9105");//允许某个域访问
+		response.setHeader("Access-Control-Allow-Credentials", "true");//允许使用cookie
+		
 		//获取登录名
 			String username = SecurityContextHolder.getContext().getAuthentication().getName();
 			System.out.println("当前登录人:"+username);
